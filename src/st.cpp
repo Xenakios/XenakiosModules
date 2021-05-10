21b3f72d5181023567d74809bf7158919031ed60
xenakios <xenakios@gmail.com>
2021-05-11 01:18:17 +0300

    ST : implemented aux parameters etc

--- src/st.cpp
+++ src/st.cpp
@@ -39,6 +39,12 @@ public:
     {
         m_pitch_env.AddNode({0.0f,0.0f,2});
         m_pitch_env.AddNode({1.0f,0.0f,2});
+
+        m_par1_env.AddNode({0.0f,0.0f,2});
+        m_par1_env.AddNode({1.0f,0.0f,2});
+
+        m_par2_env.AddNode({0.0f,0.0f,2});
+        m_par2_env.AddNode({1.0f,0.0f,2});
     }
     void process(float deltatime, float* gate,float* pitch,float* amp,float* par1, float* par2)
     {
@@ -49,6 +55,8 @@ public:
         float gain = m_amp_env->GetInterpolatedEnvelopeValue(normphase);
         *amp = rescale(gain,0.0f,1.0f,0.0f,10.0f);
         *pitch = m_pitch + m_pitch_env.GetInterpolatedEnvelopeValue(normphase);
+        *par1 = clamp(m_par1 + m_par1_env.GetInterpolatedEnvelopeValue(normphase),-5.0f,5.0f);
+        *par2 = clamp(m_par2 + m_par2_env.GetInterpolatedEnvelopeValue(normphase),-5.0f,5.0f);
         if (m_phase>=m_len)
         {
             m_available = true;
@@ -66,6 +74,12 @@ public:
         m_pitch = rescale(rack::random::uniform(),0.0f,1.0f,minpitch,maxpitch);
         float glissdest = rescale(rack::random::uniform(),0.0f,1.0f,-24.0f,24.0f);
         m_pitch_env.GetNodeAtIndex(1).pt_y = glissdest;
+        m_par1 = rescale(rack::random::uniform(),0.0f,1.0f,-5.0f,5.0f);
+        float pardest = rescale(rack::random::uniform(),0.0f,1.0f,-5.0f,5.0f);
+        m_par1_env.GetNodeAtIndex(1).pt_y = pardest;
+        m_par2 = rescale(rack::random::uniform(),0.0f,1.0f,-5.0f,5.0f);
+        pardest = rescale(rack::random::uniform(),0.0f,1.0f,-5.0f,5.0f);
+        m_par2_env.GetNodeAtIndex(1).pt_y = pardest;
         m_available = false;
     }
     float m_playProb = 1.0f;
@@ -74,14 +88,16 @@ private:
     bool m_available = true;
     breakpoint_envelope m_pitch_env;
     breakpoint_envelope* m_amp_env = nullptr;
-    breakpoint_envelope* m_par1_env = nullptr;
-    breakpoint_envelope* m_par2_env = nullptr;
+    breakpoint_envelope m_par1_env;
+    breakpoint_envelope m_par2_env;
     double m_phase = 0.0;
     double m_len = 0.5;
     float m_min_pitch = -24.0f;
     float m_max_pitch = 24.0f;
     float m_pitch = 0.0f;
     float m_glissrange = 0.0f;
+    float m_par1 = 0.0f;
+    float m_par2 = 0.0f;
 };
 
 class XStochastic : public rack::Module
@@ -95,7 +111,7 @@ public:
         ENUMS(OUT_AUX1, 16),
         ENUMS(OUT_AUX2, 16)
     };
-    int m_numAmpEnvs = 4;
+    int m_numAmpEnvs = 5;
     XStochastic()
     {
         m_amp_envelopes[0].AddNode({0.0,0.0,2});
@@ -114,6 +130,13 @@ public:
         m_amp_envelopes[3].AddNode({0.1,0.2,2});
         m_amp_envelopes[3].AddNode({0.9,0.2,2});
         m_amp_envelopes[3].AddNode({1.0,0.0,2});
+        
+        m_amp_envelopes[4].AddNode({0.00,0.0,2});
+        m_amp_envelopes[4].AddNode({0.01,1.0,2});
+        m_amp_envelopes[4].AddNode({0.50,0.05,2});
+        m_amp_envelopes[4].AddNode({0.99,1.0,2});
+        m_amp_envelopes[4].AddNode({1.00,0.0,2});
+        
         config(0,0,OUT_AUX2_LAST);
     }
     void process(const ProcessArgs& args) override
@@ -143,14 +166,18 @@ public:
             float gate = 0.0f;
             float amp = 0.0f;
             float pitch = 0.0f;
+            float par1 = 0.0f;
+            float par2 = 0.0f;
             if (m_voices[i].isAvailable()==false && m_phase>=m_voices[i].m_startPos)
             {
-                m_voices[i].process(args.sampleTime,&gate,&pitch,&amp,nullptr,nullptr);
+                m_voices[i].process(args.sampleTime,&gate,&pitch,&amp,&par1,&par2);
             }
             outputs[OUT_GATE+i].setVoltage(gate);
             pitch = pitch*(1.0f/12);
             outputs[OUT_PITCH+i].setVoltage(pitch);
             outputs[OUT_VCA+i].setVoltage(amp);
+            outputs[OUT_AUX1+i].setVoltage(par1);
+            outputs[OUT_AUX2+i].setVoltage(par2);
         }
         m_phase+=args.sampleTime;
     }
@@ -177,6 +204,8 @@ public:
             addOutput(createOutput<PJ301MPort>(Vec(5, 20+i*25), module, XStochastic::OUT_GATE+i));
             addOutput(createOutput<PJ301MPort>(Vec(30, 20+i*25), module, XStochastic::OUT_PITCH+i));
             addOutput(createOutput<PJ301MPort>(Vec(55, 20+i*25), module, XStochastic::OUT_VCA+i));
+            addOutput(createOutput<PJ301MPort>(Vec(80, 20+i*25), module, XStochastic::OUT_AUX1+i));
+            addOutput(createOutput<PJ301MPort>(Vec(105, 20+i*25), module, XStochastic::OUT_AUX2+i));
         }
     }
     ~XStochasticWidget()
