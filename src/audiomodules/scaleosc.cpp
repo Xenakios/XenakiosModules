57499020dd81306b9afd4e355b90a8cd9e1944f5
xenakios <xenakios@gmail.com>
2021-12-22 05:18:21 +0200

    Added scale

--- src/audiomodules/scaleosc.cpp
+++ src/audiomodules/scaleosc.cpp
@@ -370,6 +370,7 @@ public:
         scalafiles.push_back(dir+"/12tet.scl");
         scalafiles.push_back(dir+"/tritones.scl");
         scalafiles.push_back(dir+"/tetra01.scl");
+        scalafiles.push_back(dir+"/xenakis_jonchaies.scl");
         scalafiles.push_back(dir+"/major_chord_et.scl");
         scalafiles.push_back(dir+"/major_chord_ji.scl");
         scalafiles.push_back(dir+"/minor_chord_et.scl");
