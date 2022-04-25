af0c94ce7e469fad46eeb0571d81f13b6fcdb9e1
Xenakios <xenakios@gmail.com>
2022-04-25 18:47:28 +0300

    GRAINS : Draw different things on the 2 waveform widget instances

--- src/audiomodules/granularmodule.cpp
+++ src/audiomodules/granularmodule.cpp
@@ -706,14 +706,23 @@ public:
             int numpeaks = box.size.x - 2;
             int numchans = src.m_channels;
             float numsrcpeaks = src.peaksData[0].size();
+            float loopstartnorm = m_gm->m_eng.m_gm->m_loopstart;
+            float loopendnorm = loopstartnorm + m_gm->m_eng.m_gm->m_looplen;
+            float startpeaks = 0.0f ;
+            float endpeaks = numsrcpeaks ;
+            if (m_opts == 1)
+            {
+                startpeaks = loopstartnorm * numsrcpeaks ;
+                endpeaks = loopendnorm * numsrcpeaks ;
+            }
             float chanh = box.size.y/numchans;
             nvgBeginPath(args.vg);
             for (int i=0;i<numchans;++i)
             {
                 for (int j=0;j<numpeaks;++j)
                 {
-                    float index = rescale(j,0,numpeaks,0.0f,numsrcpeaks-1.0f);
-                    if (index<numsrcpeaks)
+                    float index = rescale(j,0,numpeaks,startpeaks,endpeaks-1.0f);
+                    if (index>=0.0f && index<numsrcpeaks)
                     {
                         int index_i = index;
                         float minp = src.peaksData[i][index_i].minpeak;
@@ -727,6 +736,49 @@ public:
                 }
             }
             nvgStroke(args.vg);
+            if (m_opts == 1)
+            {
+                nvgBeginPath(args.vg);
+                nvgStrokeColor(args.vg,nvgRGBA(0xff, 0xff, 0xff, 0xff));
+                for (int i=0;i<16;++i)
+                {
+                    float ppos = m_gm->m_eng.m_gm->getGrainSourcePosition(i);
+                    if (ppos>=0.0f)
+                    {
+                        float srcdur = m_gm->m_eng.m_gm->m_inputdur;
+                        float xcor = rescale(ppos,loopstartnorm,loopendnorm,0.0f,box.size.x);
+                        float ycor0 = box.size.y / 16 * i;
+                        float ycor1 = box.size.y / 16*(i+1);
+                        nvgMoveTo(args.vg,xcor,ycor0);
+                        nvgLineTo(args.vg,xcor,ycor1);
+                    }
+                    
+                }
+                
+                nvgStroke(args.vg);
+            }
+            if (m_opts == 0)
+            {
+                nvgBeginPath(args.vg);
+                nvgStrokeColor(args.vg,nvgRGBA(0x00, 0xff, 0xff, 0xff));
+                auto& markers = m_gm->m_eng.m_markers; 
+                for (int i=0;i<markers.size();++i)
+                {
+                    float xcor = rescale(markers[i],0.0f,1.0f,0.0f,box.size.x);
+                    nvgMoveTo(args.vg,xcor,box.size.y-5.0f);
+                    nvgLineTo(args.vg,xcor,box.size.y);
+                }
+                nvgStroke(args.vg);
+                
+                nvgBeginPath(args.vg);
+                nvgFillColor(args.vg, nvgRGBA(0x00, 0xff, 0x00, 0x80));
+                float loopstart = m_gm->m_eng.m_gm->m_actLoopstart;
+                float loopend = m_gm->m_eng.m_gm->m_actLoopend;
+                float loopw = rescale(loopend-loopstart,0.0f,1.0f,0.0f,box.size.x);
+                float xcor = rescale(loopstart,0.0f,1.0f,0.0f,box.size.x);
+                nvgRect(args.vg,xcor,0.0f,loopw,box.size.y);
+                nvgFill(args.vg);
+            }
         }
         nvgRestore(args.vg);
     }
@@ -793,11 +845,11 @@ public:
         addChild(new KnobInAttnWidget(this,"GRAIN REVERSE",XGranularModule::PAR_REVERSE,-1,-1,2*82.0f,101.0f));
         WaveFormWidget* wavew = new WaveFormWidget(m,0);
         wavew->box.pos = {1.0f,215.0f};
-        wavew->box.size = {box.size.x-2.0f,80.0f};
+        wavew->box.size = {box.size.x-2.0f,50.0f};
         addChild(wavew); 
         wavew = new WaveFormWidget(m,1);
-        wavew->box.pos = {1.0f,295.0f};
-        wavew->box.size = {box.size.x-2.0f,80.0f};
+        wavew->box.pos = {1.0f,265.0f};
+        wavew->box.size = {box.size.x-2.0f,110.0f};
         addChild(wavew);
     }
     void step() override
@@ -839,51 +891,13 @@ public:
             if (src.m_channels>0)
             {
                 
-                nvgBeginPath(args.vg);
-                nvgFillColor(args.vg, nvgRGBA(0x00, 0xff, 0x00, 0x80));
-                float loopstart = m_gm->m_eng.m_gm->m_actLoopstart;
-                float loopend = m_gm->m_eng.m_gm->m_actLoopend;
-                float loopw = rescale(loopend-loopstart,0.0f,1.0f,0.0f,box.size.x-2.0f);
-                float xcor = rescale(loopstart,0.0f,1.0f,0.0f,box.size.x-2.0f);
-                nvgRect(args.vg,xcor,250.0f,loopw,100.0f);
-                nvgFill(args.vg);
-                nvgBeginPath(args.vg);
-                nvgStrokeColor(args.vg,nvgRGBA(0xff, 0xff, 0xff, 0xff));
-                for (int i=0;i<16;++i)
-                {
-                    float ppos = m_gm->m_eng.m_gm->getGrainSourcePosition(i);
-                    if (ppos>=0.0f)
-                    {
-                        float srcdur = m_gm->m_eng.m_gm->m_inputdur;
-                        xcor = rescale(ppos,0.0f,1.0f,0.0f,box.size.x-2.0f);
-                        float ycor0 = 250.0f+100.0f/16*i;
-                        float ycor1 = 250.0f+100.0f/16*(i+1);
-                        nvgMoveTo(args.vg,xcor,ycor0);
-                        nvgLineTo(args.vg,xcor,ycor1);
-                    }
-                    
-                }
-                
-                nvgStroke(args.vg);
-
-                nvgBeginPath(args.vg);
-                nvgStrokeColor(args.vg,nvgRGBA(0x00, 0xff, 0xff, 0xff));
-                auto& markers = m_gm->m_eng.m_markers; 
-                for (int i=0;i<markers.size();++i)
-                {
-                    float xcor = rescale(markers[i],0.0f,1.0f,0.0f,box.size.x-2.0f);
-                    nvgMoveTo(args.vg,xcor,340.0f);
-                    nvgLineTo(args.vg,xcor,350.0f);
-                }
-
-                nvgStroke(args.vg);
 
                 float ppos = src.getRecordPosition();
                 if (ppos>=0.0)
                 {
                     nvgBeginPath(args.vg);
                     nvgStrokeColor(args.vg,nvgRGBA(0xff, 0x00, 0x00, 0xff));
-                    xcor = rescale(ppos,0.0f,1.0f,0.0f,box.size.x-2.0f);
+                    float xcor = rescale(ppos,0.0f,1.0f,0.0f,box.size.x-2.0f);
                     if (xcor<box.size.x)
                     {
                         nvgMoveTo(args.vg,xcor,250.0f);
