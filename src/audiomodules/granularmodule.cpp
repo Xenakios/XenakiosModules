042cae53b0eb30f01a5cf38038d6d588f4b6c6fe
xenakios <xenakios@gmail.com>
2022-06-12 22:51:17 +0300

    tweaks

--- src/audiomodules/granularmodule.cpp
+++ src/audiomodules/granularmodule.cpp
@@ -421,7 +421,7 @@ public:
             saveFile(audioFile);
 #else
             std::cout << "saving audio buffer...\n";
-            std::string audioDir = "/home/teemu/AudioStuff/GRLOOPER_RECORDINGS";
+            std::string audioDir = "/home/pi/AudioStuff/GRLOOPER_RECORDINGS";
             auto t = std::chrono::system_clock::now().time_since_epoch().count();
             std::string audioFile = audioDir+"/GrainRec_"+std::to_string(t)+".wav";
             saveFile(audioFile,m_recordBufferIndex);
@@ -1542,23 +1542,6 @@ Model* modelXGranular = createModel<XGranularModule, XGranularWidget>("XGranular
 #include <type_traits>
 #include "claphost.h"
 #include <ncurses.h>
-
-char mygetch() {
-    char buf = 0;
-    struct termios old = { 0 };
-    fflush(stdout);
-    if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
-    old.c_lflag    &= ~ICANON;   // local modes = Non Canonical mode
-    old.c_lflag    &= ~ECHO;     // local modes = Disable echo. 
-    old.c_cc[VMIN]  = 1;         // control chars (MIN value) = 1
-    old.c_cc[VTIME] = 0;         // control chars (TIME value) = 0 (No time)
-    if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
-    if (read(0, &buf, 1) < 0) perror("read()");
-    old.c_lflag    |= ICANON;    // local modes = Canonical mode
-    old.c_lflag    |= ECHO;      // local modes = Enable echo. 
-    if (tcsetattr(0, TCSADRAIN, &old) < 0) perror ("tcsetattr ~ICANON");
-    return buf;
- }
 
 class AudioEngine
 {
