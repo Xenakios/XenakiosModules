281af26f66daeef58136ebc81bec1fc057f7df0f
xenakios <xenakios@gmail.com>
2022-06-22 06:11:32 +0300

    initial work to be able to use threadsafe event container but postpone for now

--- src/audiomodules/claphosting/claphost.cpp
+++ src/audiomodules/claphosting/claphost.cpp
@@ -18,6 +18,9 @@ static clap_host xen_host_static{
     CLAP_VERSION_INIT, nullptr,       "GRLOOPERHOST",     "Xenakios", "no website",
     "0.0.0",           get_extension, request_restart, request_process,     request_callback};
 
+#define USE_CHOC_RB 0
+
+#if !USE_CHOC_RB
 struct micro_input_events
 {
     static constexpr int max_evt_size = 10 * 1024;
@@ -64,7 +67,56 @@ struct micro_input_events
         mie->sz = 0;
     }
 };
+#else
+struct micro_input_events
+{
+    static constexpr int max_evt_size = 10 * 1024;
+    static constexpr int max_events = 4096;
+    uint8_t data[max_evt_size * max_events];
+    uint32_t sz{0};
+    choc::fifo::SingleReaderSingleWriterFIFO<uint8_t> fifo;
+    static void setup(clap_input_events *evt)
+    {
+        auto mie = new micro_input_events;
+        mie->fifo.reset(16384);
+        evt->ctx = mie;
+        evt->size = size;
+        evt->get = get;
+    }
+    static void destroy(clap_input_events *evt) { delete (micro_input_events *)evt->ctx; }
 
+    static uint32_t size(const clap_input_events *e)
+    {
+        auto mie = static_cast<micro_input_events *>(e->ctx);
+        return mie->fifo.getUsedSlots();
+    }
+
+    static const clap_event_header_t *get(const clap_input_events *e, uint32_t index)
+    {
+        auto mie = static_cast<micro_input_events *>(e->ctx);
+        //assert(index >= 0);
+        //assert(index < max_events);
+        uint8_t *ptr = &(mie->data[index * max_evt_size]);
+        return reinterpret_cast<clap_event_header_t *>(ptr);
+    }
+
+    template <typename T> static void push(clap_input_events *e, const T &t)
+    {
+        auto mie = static_cast<micro_input_events *>(e->ctx);
+        //assert(t.header.size <= max_evt_size);
+        //assert(mie->sz < max_events - 1);
+        uint8_t *ptr = &(mie->data[mie->sz * max_evt_size]);
+        memcpy(ptr, &t, t.header.size);
+        mie->sz++;
+    }
+
+    static void reset(clap_input_events *e)
+    {
+        auto mie = static_cast<micro_input_events *>(e->ctx);
+        mie->sz = 0;
+    }
+};
+#endif
 struct micro_output_events
 {
     static constexpr int max_evt_size = 1024;
